28978acce73e76717c650ce7a5325fd84e098beb
RMSIRathnayaka <135612054+RMSIRathnayaka@users.noreply.github.com>
2023-06-28 14:45:07 +0530

    practical 05

--- main.c
+++ main.c
@@ -1,82 +1,91 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define PI 3.14159
 
 int main()
 {
-    int a;
-    printf("Enter a number:");
-    scanf("%d",&a);
-    if(a%2==0){
-        printf("%d is an even number.\n",a);
-    }else {
-        printf("%d is a odd number.\n",a);
-    }
-    printf("\n");
-
-    int a;
-    printf("Enter a number:");
-    scanf("%d",&a);
-    switch(a%2)
-    {
-        case 0:printf("%d is an even number\n",a);break;
-        case 1:printf("%d is a odd number\n",a);break;
-    }
-    return 0;
-
-    int n1,n2;
-    char o;
-    printf("Enter two numbers:");
-    scanf("%d %d",&n1,&n2);
-
-    printf("Enter the operator:");
-    scanf("%c",&o);
-
-    switch(o)
-    {
-    case '+':printf("Addition = %d\n",n1+n2);break;
-    case '-':printf("Subtraction = %d\n",n1-n2);break;
-    case '*':printf("Multiplication = %d\n",n1*n2);break;
-    case '/':printf("Division = %d\n",n1/n2);break;
-    default:printf("Invalid Operator\n");
-    }
-    return 0;
-
-    int choice;
-    float r,d,c,a;
-    printf("Menu driven calculator\n");
-
-    printf("Enter the radius of the circle");
-    scanf("%f",&r);
-
-    printf("Selection a calculation");
-    printf("1.Calculate diameter\n");
-    printf("2.Calculate circumference\n");
-    printf("3.Calculate area\n");
-
-    printf("Enter your choice(1-3):");
-    scanf("%d",&choice);
-    switch(choice)
-    {
-        case 1:printf("Result-%.2f\n",2*r);break;
-        case 2:printf("Result-%.2f\n",2*PI*r);break;
-        case 3:printf("Result-%.2f\n",PI*r*r);break;
-        default:printf("Invalid choice\n");
+    int i=0;
+    while(i<=100){
+        printf("%d ",i);
+        i++;
     }
     return 0;
 
 
-    char x;
-    printf("Enter a character: ");
-    scanf("%c",&x);
-    switch(x)
-    {
-    case 'a':printf("a is a vowel");break;
-    case 'e':printf("e is a vowel");break;
-    case 'i':printf("i is a vowel");break;
-    case 'o':printf("o is a vowel");break;
-    case 'u':printf("u is a vowel");break;
-    default:printf("%c is not a vowel \n",x);
+    int i=0'
+    do{
+        printf("%d ",i);
+        i++;
+    }while (i<=100);
+    return 0;
+
+
+    int i;
+    for(i=0;i<=100;i++){
+        printf("%d ",i);
     }
+    return 0;
+
+
+    int i,marks[10];
+    int total = 0;
+    float avg;
+
+    printf("Enter 10 marks: ");
+    for (i = 0; i < 10; i++) {
+        scanf("%d", &marks[i]);
+        total += marks[i];
+    }
+
+    avg=total/10;
+    if (avg < 50) {
+        printf("Fail!\n");
+    } else {
+        printf("Pass!\n");
+    }
+
+    return 0;
+
+
+    int number;
+    int i=1, fact=1;
+
+    printf("Enter a number: ");
+    scanf("%d",&number);
+
+    while(i<=number){
+        fact*=i;
+        i++;
+    }
+    printf("Factorial is %d",fact);
+
+
+    int number, total=0, reminder;
+    printf("Enter number: ");
+    scanf("%d",&number);
+
+    while(number!=0){
+        reminder=number%10;
+        total+=reminder;
+        number=number/10;
+    }
+    printf("The output is %d",total);
+
+
+    int n;
+    int r=0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    do {
+        r= r*10+n%10;
+        n /= 10;
+    } while (n > 0);
+
+    printf("Reversed: %d\n",r);
+
+
+
+
 
 }
